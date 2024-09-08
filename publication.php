af92a1cd231f78c6631027afd9229585c261ad62
artur <arturzadelyan@bk.ru>
2024-09-08 15:20:46 -0700

    init

---
+++ publication.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html lang="hy">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="description" content="Ampera about project">
+    <title>Ampera project</title>
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <link rel="stylesheet" href="css/publication/index.css">
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="css/publication/index-l.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="css/publication/index-m.css">
+    <?php
+    include 'templates/favicons.php'
+    ?>
+</head>
+
+<body>
+    <?php
+    include 'templates/header.php'
+    ?>
+    <div class="content">
+        <?php
+        include 'templates/main.php'
+        ?>
+        <div class="breadcrumbs">
+            <div class="page_container">
+                <ul>
+                    <li><a href="services.php">Հրապարակումներ</a></li>
+                    <li>
+                        <div>Հաշվետվություններ</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="publication_page">
+            <div class="page_container">
+                <h2 class=" section_title">Հրապարակումներ</h2>
+                <div class="result_list">
+                    <div class="result_block">
+                        <div class="result_title">Բարձրարժեք մշակաբույսերի դիվերսիֆիկացիայի ընդլայնում. Անասնաբուծության կայուն ինտենսիվացման և պատրաստվածության կենսունակության գնահատում</div>
+                        <ul>
+                            <li>
+                                <span class="title_block">Ոլորտ:</span>
+                                <span class="link_description">Գյուղատնտեսություն, գյուղատնտեսական ծառայություններ և ենթակառուցվածքներ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Ծառայություն:</span>
+                                <span class="link_description">Տնտեսական և սոցիալական հետազոտություններ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Պատվիրատու:</span>
+                                <span class="link_description">ԱԶԲ</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="result_block">
+                        <div class="result_title">Բարձրարժեք մշակաբույսերի դիվերսիֆիկացիայի ընդլայնում. Անասնաբուծության կայուն ինտենսիվացման և պատրաստվածության կենսունակության գնահատում</div>
+                        <ul>
+                            <li>
+                                <span class="title_block">Ոլորտ</span>
+                                <span class="link_description">Գյուղատնտեսություն, գյուղատնտեսական ծառայություններ և ենթակառուցվածքներ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Ծառայություն</span>
+                                <span class="link_description">Տնտեսական և սոցիալական հետազոտություններ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Պատվիրատու:</span>
+                                <span class="link_description">ԱԶԲ</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="result_block">
+                        <div class="result_title">Բարձրարժեք մշակաբույսերի դիվերսիֆիկացիայի ընդլայնում. Անասնաբուծության կայուն ինտենսիվացման և պատրաստվածության կենսունակության գնահատում</div>
+                        <ul>
+                            <li>
+                                <span class="title_block">Ոլորտ</span>
+                                <span class="link_description">Գյուղատնտեսություն, գյուղատնտեսական ծառայություններ և ենթակառուցվածքներ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Ծառայություն</span>
+                                <span class="link_description">Տնտեսական և սոցիալական հետազոտություններ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Պատվիրատու:</span>
+                                <span class="link_description">ԱԶԲ</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="result_block">
+                        <div class="result_title">Բարձրարժեք մշակաբույսերի դիվերսիֆիկացիայի ընդլայնում. Անասնաբուծության կայուն ինտենսիվացման և պատրաստվածության կենսունակության գնահատում</div>
+                        <ul>
+                            <li>
+                                <span class="title_block">Ոլորտ</span>
+                                <span class="link_description">Գյուղատնտեսություն, գյուղատնտեսական ծառայություններ և ենթակառուցվածքներ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Ծառայություն</span>
+                                <span class="link_description">Տնտեսական և սոցիալական հետազոտություններ</span>
+                            </li>
+                            <li>
+                                <span class="title_block">Պատվիրատու:</span>
+                                <span class="link_description">ԱԶԲ</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include 'templates/footer.php'
+    ?>
+
+    <script src="js/swiper.min.js"></script>
+    <script type="module" src="js/index.js"></script>
+</body>
+
+</html>
---
+++ services.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML>
+<html lang="hy">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="description" content="Ampera about project">
+    <title>Ampera project</title>
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <link rel="stylesheet" href="css/services/index.css">
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="css/services/index-l.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="css/services/index-m.css">
+    <?php
+    include 'templates/favicons.php'
+    ?>
+</head>
+
+<body>
+    <?php
+    include 'templates/header.php'
+    ?>
+    <div class="content">
+        <?php
+        include 'templates/main.php'
+        ?>
+        <div class="breadcrumbs">
+            <div class="page_container">
+                <ul>
+                    <li><a href="index.php">Ծառայություններ</a></li>
+                    <li>
+                        <div> Տնտեսական ԵՎ սոցիալական հետազոտություններ</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="services_page">
+            <div class="page_container">
+                <h2 class="section_title ">Ծառայություններ</h2>
+                <ul class="services_list">
+                    <li>
+                        <div class="type_list">Ծրագրերի կառավարում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Տնտեսական և սոցիալական հետազոտություններ</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Հանրային հարցումներ</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Բիզնես և ներդրումային պլանավորում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Ռազմավարական պլանավորում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Մեթոդաբանության և ուղեցույցների մշակում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Շուկայավարում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Ֆինանսական պրոդուկտների մշակում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Ուսուցում և իրազեկում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Մոնիտորինգ և գնահատում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                    <li>
+                        <div class="type_list">Ծրագրերի մշակում և իրականացում</div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ավելին</a></div>
+                        <div class="link_item"><a href="#" class="icon_arrow_left">Տեսնել ծրագրերը</a></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include 'templates/footer.php'
+    ?>
+
+    <script src="js/swiper.min.js"></script>
+    <script type="module" src="js/index.js"></script>
+</body>
+
+</html>
